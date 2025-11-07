6e8b01983502ce9b5d32a49c44672b7c7075e465
ryanpurnomo83 <ryanpurnomo83@gmail.com>
2025-11-08 00:37:05 +0700

    fourth commit

--- app.py
+++ app.py
@@ -1,13 +1,30 @@
 import os 
 from supabase import create_client , Client
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, url_for
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.1.4:5173", "http://192.168.1.4:8080"]}})
 
 url: str = "https://otkvvqgnyojmuwagbbpq.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90a3Z2cWdueW9qbXV3YWdiYnBxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI0NTMzNjQsImV4cCI6MjA3ODAyOTM2NH0.c_cH8fntvWoB5wI2m6ETFz2pVZedPSOoUBKEh5rGZrs"
 supabase: Client = create_client(url, key)
+
+@app.route("/")
+def welcome():
+    flask_logo_url = url_for('static', filename='flask_logo.png')
+    horus_logo_url = url_for('static', filename='horus_logo.png')
+    return f"""
+    <br>
+    <div style='display: flex;flex-direction: row; justify-content: center;'>
+        <img src='{flask_logo_url}' alt='Flask Logo' width='30%'>
+        <br>
+        <img src='{horus_logo_url}' alt='Flask Logo' width='30%'>
+    </div>
+    <br>
+    <p style='text-align:center;'>Selamat Datang di Flask</p>
+    """
 
 @app.post("/users/register")
 def register_user():
@@ -117,7 +134,7 @@ def delete_user(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
     
 
 # https://otkvvqgnyojmuwagbbpq.supabase.co
